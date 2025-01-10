eba9a876fe5c2d8995c0f7dfd74bfda9b5acd7e5
psalmistcom <dadewale9@gmail.com>
2025-01-10 23:38:57 +0100

    started the calender

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,7 @@
+import Announcements from "@/components/Announcements"
 import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
+import EventCalendar from "@/components/EventCalendar"
 import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
@@ -37,7 +39,10 @@ const Adminpage = () => {
       </div>
 
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   )
 }
---
+++ src/components/Announcements.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Announcements = () => {
+    return (
+        <div>
+            annoucement goes here
+        </div>
+    )
+}
+
+export default Announcements
